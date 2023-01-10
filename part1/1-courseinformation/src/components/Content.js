24b660d9aebae807e5f65c56d31c685a0483ffd7
Lamin Barrow <laminbarrow@gmail.com>
2023-01-10 13:25:58 +0000

    Feat: course information step 1 - 2

---
+++ part1/1-courseinformation/src/components/Content.js
@@ -0,0 +1,12 @@
+import Part from "./Part"
+const Content = (props) => {
+    return (
+    <div>
+        <Part name={props.part1} number={props.exercises1} />
+        <Part name={props.part2} number={props.exercises2} />
+        <Part name={props.part3} number={props.exercises3} />
+    </div>
+    )
+}
+
+export default Content
