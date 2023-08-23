a16c380a59cd1e2e551fdcdc586d1dddabe9bf81
Lamin Barrow <laminbarrow@gmail.com>
2023-08-23 20:15:36 +0000

    feat: Added table structure - unicafe step6

--- part1/1-unicafe/src/components/Statistics.jsx
+++ part1/1-unicafe/src/components/Statistics.jsx
@@ -1,17 +1,19 @@
 import StatisticLine from "./StatisticLine";
 
 // Statistics
-const Statistics = ( props ) => {
-    return (
-      <>
+const Statistics = (props) => {
+  return (
+    <table>
+      <tbody>
         <StatisticLine text="good" value={props.good} />
         <StatisticLine text="neutral" value={props.neutral} />
         <StatisticLine text="bad" value={props.bad} />
         <StatisticLine text="all" value={props.all} />
         <StatisticLine text="average" value={props.all} />
         <StatisticLine text="positive" value={props.positive + " %"} />
-      </>
-    );
-  };
+      </tbody>
+    </table>
+  );
+};
 
-  export default Statistics
+export default Statistics;
