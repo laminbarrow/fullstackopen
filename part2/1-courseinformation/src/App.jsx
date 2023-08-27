9b10a58ca8a875363f38aeebbd4483b71854b122
Lamin Barrow <laminbarrow@gmail.com>
2023-08-27 21:08:18 +0000

    Feat: object destructuring to get the total sum

--- part2/1-courseinformation/src/App.jsx
+++ part2/1-courseinformation/src/App.jsx
@@ -1,21 +1,33 @@
 const Course = ({ course }) => {
+  const parts = course.parts
+
+  // destructuring parts to extract exercises
+  const total = parts.reduce((acc, {exercises}) => {
+    return acc + exercises
+  }, 0)
+
   return (
     <>
       <Header course={course.name} />
-      <Content parts={course.parts} />
+      <Content parts={parts} />
+      <Total sum={total} />
     </>
-  );
-};
+  )
+}
 
-const Header = ({ course }) => <h1>{course}</h1>;
+const Header = ({ course }) => <h1>{course}</h1>
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+const Total = ({ sum }) => {
+  return(
+    <p>Number of exercises {sum}</p>
+  )
+}
 
 const Part = ({ part }) => (
   <p>
     {part.name} {part.exercises}
   </p>
-);
+)
 
 const Content = ({ parts }) => parts.map((part) => <Part key={part.id} part={part} />)
 
@@ -40,9 +52,9 @@ const App = () => {
         id: 3,
       },
     ],
-  };
+  }
 
   return <Course course={course} />;
-};
+}
 
-export default App;
+export default App
