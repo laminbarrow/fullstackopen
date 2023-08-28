0e8ebcf62b0a9208187265b3a5ca50f7632a497b
Lamin Barrow <laminbarrow@gmail.com>
2023-08-28 19:23:50 +0000

    Feat:  The Phonebook Step3 (You can now added a contact number as well) We are now also checking for name uniqueness based on an object

--- part2/2-phonebook/src/App.jsx
+++ part2/2-phonebook/src/App.jsx
@@ -3,24 +3,41 @@ import './App.css'
 
 const Contact = (prop) => {
   return (
-    <div>{prop.name}</div>
+    <div>{prop.name} - {prop.number}</div>
   )
 }
 
 function App() {
   const [contacts, setContacts] = useState([])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addNewContact = (event) => {
     event.preventDefault()
 
+    const newContact = {
+      name: newName,
+      number: newNumber
+    }
+    //make sure this contact was not added before
+    if(contacts.find(({name}) => name === newName)){
+      alert(`${newName} is already added to phonebook`);
+
+      //exit the function
+      return
+    }
     // add new contact
-    setContacts(contacts.concat(newName))
+    setContacts(contacts.concat(newContact))
     setNewName('')
+    setNewNumber('')
   }
 
-  const handleNoteChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
   }
 
   return (
@@ -28,7 +45,10 @@ function App() {
         <h2>Phonebook</h2>
         <form onSubmit={addNewContact}>
           <div>
-            name: <input placeholder='new contact..' value={newName} onChange={handleNoteChange} />
+            name: <input placeholder='new contact..' value={newName} onChange={handleNameChange} />
+          </div>
+          <div>
+            number: <input placeholder='phone number' value={newNumber} onChange={handleNumberChange} />
           </div>
           <div>
             <button type="submit">add</button>
@@ -37,7 +57,7 @@ function App() {
       <h2>Numbers</h2>
       {
         contacts.map(contact => (
-          <Contact key={contact} name={contact} />
+          <Contact key={contact.name} name={contact.name} number={contact.number} />
         ) )
       }
     </div>
@@ -45,3 +65,4 @@ function App() {
 }
 
 export default App
+
